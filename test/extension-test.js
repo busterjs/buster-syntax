545e95cbb6841cab2e85a32b4b8721bdfba3697e
Christian Johansen <christian@gitorious.org>
2012-02-13 07:49:58 +0100

    beforeRun hook now runs on config, not resourceSet
      - Only syntax check user resources, not framework

--- test/extension-test.js
+++ test/extension-test.js
@@ -1,68 +1,87 @@
 var buster = require("buster");
-var ext = require("../lib/buster-syntax").extension;
-var resourceSet = require("buster-resources").resourceSet;
+var syntax = require("../lib/buster-syntax").extension;
+var config = require("buster-configuration");
 var analyzer = require("buster-analyzer");
+
+function process(group, then, errBack) {
+    group.resolve().then(function (resourceSet) {
+        resourceSet.serialize().then(then, errBack);
+    }, errBack);
+}
 
 buster.testCase("Syntax extension", {
     setUp: function () {
-        this.resourceSet = resourceSet.create();
+        this.config = config.create();
         this.analyzer = analyzer.create();
         this.listeners = { fatal: this.spy(), error: this.spy() };
         this.analyzer.on("fatal", this.listeners.fatal);
         this.analyzer.on("error", this.listeners.error);
     },
 
-    "adds processor to resource set": function () {
-        var syntax = ext.create();
-        this.spy(this.resourceSet, "addProcessor");
-        syntax.beforeRun(this.resourceSet, this.analyzer);
-
-        assert.calledOnce(this.resourceSet.addProcessor);
-    },
-
     "flags fatal on syntax error": function (done) {
-        var syntax = ext.create();
-        syntax.beforeRun(this.resourceSet, this.analyzer);
+        var group = this.config.addGroup("Some tests", {
+            resources: [{ path: "/buster.js", content: "va a = 42;" }],
+            sources: ["/buster.js"]
+        });
 
-        this.resourceSet.addResource({
-            path: "/buster.js",
-            content: "va a = 42;"
-        }).then(function (resource) {
-            resource.content().then(done(function (content) {
-                assert.calledOnce(this.listeners.fatal);
-                assert.calledWith(this.listeners.fatal,
-                                  "Syntax error in /buster.js");
-            }.bind(this)));
-        }.bind(this));
+        syntax.create().beforeRun(group, this.analyzer);
+
+        process(group, done(function (resource) {
+            assert.calledOnce(this.listeners.fatal);
+            assert.calledWith(this.listeners.fatal,
+                              "Syntax error in /buster.js");
+        }.bind(this)));
     },
 
     "flags error on reference error": function (done) {
-        var syntax = ext.create();
-        syntax.beforeRun(this.resourceSet, this.analyzer);
+        var group = this.config.addGroup("Some tests", {
+            resources: [{ path: "/buster.js", content: "var a = $('div');" }],
+            sources: ["/buster.js"]
+        });
 
-        this.resourceSet.addResource({
-            path: "/buster.js",
-            content: "var a = $('div');"
-        }).then(function (resource) {
-            resource.content().then(done(function (content) {
-                assert.calledOnce(this.listeners.error);
-                assert.calledWith(this.listeners.error,
-                                  "ReferenceError in /buster.js");
-            }.bind(this)));
-        }.bind(this));
+        syntax.create().beforeRun(group, this.analyzer);
+
+        process(group, done(function (resource) {
+            assert.calledOnce(this.listeners.error);
+            assert.calledWith(this.listeners.error,
+                              "ReferenceError in /buster.js");
+        }.bind(this)));
     },
 
     "skips reference error if configured thusly": function (done) {
-        var syntax = ext.create({ ignoreReferenceErrors: true });
-        syntax.beforeRun(this.resourceSet, this.analyzer);
+        var group = this.config.addGroup("Some tests", {
+            resources: [{ path: "/buster.js", content: "var a = $('div');" }],
+            sources: ["/buster.js"]
+        });
 
-        this.resourceSet.addResource({
-            path: "/buster.js",
-            content: "var a = $('div');"
-        }).then(function (resource) {
-            resource.content().then(done(function (content) {
-                refute.called(this.listeners.error);
-            }.bind(this)));
-        }.bind(this));
+        syntax.create({
+            ignoreReferenceErrors: true
+        }).beforeRun(group, this.analyzer);
+
+        process(group, done(function (resource) {
+            refute.called(this.listeners.error);
+        }.bind(this)));
+    },
+
+    "flags fatal on all user sources, not framework": function (done) {
+        var group = this.config.addGroup("Some tests", {
+            resources: [
+                { path: "/buster.js", content: "va a = 42;" },
+                { path: "/buster2.js", content: "va a = 42;" },
+                { path: "/buster3.js", content: "va a = 42;" },
+                { path: "/buster4.js", content: "va a = 42;" }
+            ],
+            libs: ["/buster.js"],
+            sources: ["/buster.js"],
+            testLibs: ["/buster.js"],
+            tests: ["/buster.js"]
+        });
+
+        group.bundleFramework();
+        syntax.create().beforeRun(group, this.analyzer);
+
+        process(group, done(function (resource) {
+            assert.equals(this.listeners.fatal.callCount, 4);
+        }.bind(this)), buster.log);
     }
 });
