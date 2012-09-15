51e58ac5416473c21b4d2cd0d56c71f2eca238d2
Christian Johansen <christian@cjohansen.no>
2012-09-15 12:10:34 +0200

    API change: create -> configure
      - Make .check work without calling configure first

--- test/syntax-test.js
+++ test/syntax-test.js
@@ -6,15 +6,15 @@ var syntax = require("../lib/syntax");
 
 testCase("Syntax", {
     "passes syntactically valid code": function () {
-        assert(syntax.create().check("var a = 42;").ok);
+        assert(syntax.configure().check("var a = 42;").ok);
     },
 
     "passes syntactically valid code with file name": function () {
-        assert(syntax.create().check("var a = 42;", "booya.js").ok);
+        assert(syntax.configure().check("var a = 42;", "booya.js").ok);
     },
 
     "fails syntactically invalid code": function () {
-        var result = syntax.create().check("va a = 42;");
+        var result = syntax.configure().check("va a = 42;");
         refute(result.ok);
         assert.equals(result.errors, [{
             type: syntax.SYNTAX_ERROR,
@@ -27,7 +27,7 @@ testCase("Syntax", {
     },
 
     "formats syntax error nicely": function () {
-        var result = syntax.create().check("va a = 42;");
+        var result = syntax.configure().check("va a = 42;");
 
         assert.equals(result.errors, [{
             type: syntax.SYNTAX_ERROR,
@@ -40,7 +40,7 @@ testCase("Syntax", {
     },
 
     "formats syntax error with file name": function () {
-        var result = syntax.create().check("va a = 42;", "omg.js");
+        var result = syntax.configure().check("va a = 42;", "omg.js");
 
         assert.match(result.errors, [{
             file: "omg.js",
@@ -50,17 +50,17 @@ testCase("Syntax", {
     },
 
     "recognizes browser globals": function () {
-        assert(syntax.create().check("document.createElement('div');").ok);
+        assert(syntax.configure().check("document.createElement('div');").ok);
     },
 
     "recognizes globals introduced by previously loaded script": function () {
-        var checker = syntax.create();
+        var checker = syntax.configure();
         checker.check("jQuery = function () {};");
         assert(checker.check("var a = jQuery('div');").ok);
     },
 
     "does not recognize undefined globals": function () {
-        var checker = syntax.create();
+        var checker = syntax.configure();
         checker.check("jQuery = function () {};");
         var result = checker.check("var a = $('div');");
         refute(result.ok);
@@ -68,18 +68,28 @@ testCase("Syntax", {
     },
 
     "ignores reference errors": function () {
-        var checker = syntax.create({ ignoreReferenceErrors: true });
+        var checker = syntax.configure({ ignoreReferenceErrors: true });
         var result = checker.check("var a = $('div');");
         assert(result.ok);
     },
 
     "does not fail on file ending in comment": function () {
-        var checker = syntax.create();
+        var checker = syntax.configure();
         var result = checker.check("jQuery = function () {};\n// Ok");
         assert(result.ok);
 
-        checker = syntax.create({ ignoreReferenceErrors: true });
+        checker = syntax.configure({ ignoreReferenceErrors: true });
         result = checker.check("jQuery = function () {};\n// Ok");
+        assert(result.ok);
+    },
+
+    "passes valid code without configuration": function () {
+        var result = syntax.check("jQuery = function () {};\n// Ok");
+        assert(result.ok);
+    },
+
+    "fails invalid code without configuration": function () {
+        var result = syntax.check("jQuery = function () {};\n// Ok");
         assert(result.ok);
     }
 });
